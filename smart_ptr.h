6bd3acbcbbb9e9a58d6b4d75ef63978728bd8e8f
Erwin <274868686@qq.com>
2021-02-04 09:15:40 +0800

    ...

---
+++ smart_ptr.h
@@ -0,0 +1,28 @@
+/*
+ * smart_ptr.h
+ *
+ *  Created on: Feb 4, 2021
+ *      Author: Erwin
+ */
+
+#ifndef SMRAT_PTR_H_
+#define SMART_PTR_H_
+
+template <typename T>
+class smart_ptr {
+ public:
+  // To do
+  explicit smart_ptr(T* ptr = nullptr) : ptr(_ptr) {}
+  ~smart_ptr() { delete ptr_; }
+
+  T* get const { return ptr_; }
+
+  T& operator*() const { return *ptr; }
+  T* operator(->) const { return ptr_; }
+  operator bool() const { return ptr_; }
+
+ private:
+  T* ptr_;
+};
+
+#endif  // SMART_PTR_H_
